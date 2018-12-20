6abd6655c1e35b13f8de56e2db657e1faa7ceae9
Anik Das <anik.das@pathao.com>
2018-12-20 16:43:18 +0600

    Some Conditional operators tutorail

--- src/try.kt
+++ src/try.kt
@@ -79,4 +79,28 @@ long string"""
     for (x in rng3) println("rng3: $x")
     for (x in tenTo1.reversed()) println("Reverse: $x")
     println("---------------------------------------------------")
+
+
+//    Conditions
+    val  age = 8
+    if (age < 5) {
+        println("Go to Preschool")
+    } else if (age == 5) {
+        println("Go to Kindergarten")
+    } else if ((age > 5) && (age <= 17)) {
+        val grade = age - 5
+        println("Go to Grade $grade")
+    } else {
+        println("Go to College")
+    }
+
+    when(age) {
+        0,1,2,4 -> println("Go to Preschool")
+        5 -> println("Got to Kindergarten")
+        in 6..17 -> {
+            val grade = age -5
+            println("Go to Grade $grade")
+        }
+        else -> println("Go to College")
+    }
 }
