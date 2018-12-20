3c283532acd62971e60817fc9cee33c6fd16346d
Anik Das <anik.das@pathao.com>
2018-12-20 15:42:29 +0600

    More variable trying

--- src/try.kt
+++ src/try.kt
@@ -12,4 +12,10 @@ fun main(args: Array<String>) {
     var dblNum1: Double = 1.1111111111111111
     var dblNum2: Double = 1.1111111111111111
     println("Sum: "+(dblNum1 + dblNum2))
+
+    if (true is Boolean) {
+        print("true is boolean \n")
+    }
+    var letterGrade: Char = 'A'
+    println("A is a Char: ${letterGrade is Char}")
 }
