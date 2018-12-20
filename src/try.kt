516e48217bdd0ec385f623e8fb89a81110535487
Anik Das <anik.das@pathao.com>
2018-12-20 15:39:50 +0600

    Kotlin variable try

--- src/try.kt
+++ src/try.kt
@@ -1,3 +1,15 @@
 fun main(args: Array<String>) {
     println("Hello world")
+
+    val name = "Anik"
+    var myAge = 22
+    var bigInt: Int = Int.MAX_VALUE
+    var smallInt: Int = Int.MIN_VALUE
+
+    println("Big Int: "+bigInt)
+    println("Small Int: $smallInt")
+
+    var dblNum1: Double = 1.1111111111111111
+    var dblNum2: Double = 1.1111111111111111
+    println("Sum: "+(dblNum1 + dblNum2))
 }
