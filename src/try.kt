0cef2f34f429e7d123472598ec91d4463c38070c
Anik Das <anik.das@pathao.com>
2018-12-24 14:14:26 +0600

    Exception handling tutorial

--- src/try.kt
+++ src/try.kt
@@ -1,3 +1,4 @@
+import java.lang.IllegalArgumentException
 import java.util.*
 
 fun main(args: Array<String>) {
@@ -197,7 +198,17 @@ long string"""
     println("---------------------------------------------------")
 
 
-    
+//    Exception handling
+    val divisor = 0
+    try {
+        if (divisor == 0) {
+            throw IllegalArgumentException("Can't Divide by Zero")
+        } else {
+            println("5 / $divisor = ${5/divisor}")
+        }
+    } catch (e: IllegalArgumentException) {
+        println("${e.message}")
+    }
 }
 
 
