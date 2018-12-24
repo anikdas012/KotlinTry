97bdf1090385c941cafaa3b979c708aa15ec8017
Anik Das <anik.das@pathao.com>
2018-12-24 13:07:06 +0600

    Done with function tutorials

--- src/try.kt
+++ src/try.kt
@@ -171,6 +171,14 @@ long string"""
 
     val mult3 = makeMathFunc(3)
     println("5 *3 = ${mult3(5)}")
+
+    val multiply2 = {num1: Int -> num1 * 2}
+    val numList2 = arrayOf(1,2,3,4,5)
+    mathOnList(numList2, multiply2)
+    println("---------------------------------------------------")
+
+
+    
 }
 
 
@@ -195,3 +203,9 @@ fun fact(x: Int): Int {
 }
 
 fun makeMathFunc(num1: Int): (Int) ->Int = {num2 -> num1 * num2}
+
+fun mathOnList(numList: Array<Int>, myFunc: (num: Int) -> Int) {
+    for (num in numList) {
+        println("MathOnList ${myFunc(num)}")
+    }
+}
