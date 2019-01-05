9da2f6d8fe4f2f3f97afcd0cea62bc92c2ef234a
Anik Das <anik.das@pathao.com>
2019-01-05 18:00:57 +0600

    Added 3rd constructor to Student class

--- src/ConstructorTry.kt
+++ src/ConstructorTry.kt
@@ -2,19 +2,25 @@
 fun main(args: Array<String>) {
     var student = Student("Anik", 14301002)
 
-    println(student.id)
+    println("Student id is ${student.id} and subject is ${student.subject}")
 }
 
 class Student(var name: String) {
 
     var id: Int = -1
+    var subject: String? = null
 
     init {
-        println("Student name is $name and id is $id")
+        println("Student name is $name and id is $id and subject is $subject")
     }
 
     constructor(name: String, id: Int): this(name) {
 //        Secondary Constructor's body is always called after init block
         this.id = id
+    }
+
+    constructor(name: String, id: Int, subject: String): this(name) {
+        this.id = id
+        this.subject = subject
     }
 }
