0766ad5d14ef31ad0be311f43e368652cc7629b8
Anik Das <anik.das@pathao.com>
2019-01-05 17:55:26 +0600

    Using the secondary constructor of Student class

--- src/ConstructorTry.kt
+++ src/ConstructorTry.kt
@@ -1,10 +1,20 @@
 
 fun main(args: Array<String>) {
-    var student = Student("Anik")
+    var student = Student("Anik", 14301002)
+
+    println(student.id)
 }
 
 class Student(var name: String) {
+
+    var id: Int = -1
+
     init {
-        println("Student name is $name")
+        println("Student name is $name and id is $id")
+    }
+
+    constructor(name: String, id: Int): this(name) {
+//        Secondary Constructor's body is always called after init block
+        this.id = id
     }
 }
