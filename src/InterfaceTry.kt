c4e40d19f22b95216ef7400101fd637ca18825ba
Anik Das <anik.das@pathao.com>
2019-01-06 12:01:05 +0600

    Created an Abstract class and a Normal class
     * Normal class inherites from the Abstract class

---
+++ src/InterfaceTry.kt
@@ -0,0 +1,14 @@
+abstract class View {       //This is a Abstract class
+    abstract var viewName: String
+    abstract fun printDetails()
+}
+
+open class TouchButton(var name: String, override var viewName: String) : View() {      //This is a Normal class inheriting a Abstract class
+    override fun printDetails() {
+        println("In TouchButton class name is $name and View class viewName is $viewName")
+    }
+
+    init {
+        printDetails()
+    }
+}
