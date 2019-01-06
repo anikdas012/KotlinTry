3e6c11368461f20cf0b1e6544cc661ee764d5ea6
Anik Das <anik.das@pathao.com>
2019-01-06 12:03:34 +0600

    Changed Normal class name and comment of Abstract class

--- src/InterfaceTry.kt
+++ src/InterfaceTry.kt
@@ -1,9 +1,9 @@
-abstract class View {       //This is a Abstract class
+abstract class View {       //This is an Abstract class
     abstract var viewName: String
     abstract fun printDetails()
 }
 
-open class TouchButton(var name: String, override var viewName: String) : View() {      //This is a Normal class inheriting a Abstract class
+open class Button(var name: String, override var viewName: String) : View() {      //This is a Normal class inheriting a Abstract class
     override fun printDetails() {
         println("In TouchButton class name is $name and View class viewName is $viewName")
     }
